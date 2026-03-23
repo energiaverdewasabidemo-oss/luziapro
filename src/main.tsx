2fcee994a0a1837aec358f3e4b1718f9badd08ed
energiaverdewasabidemo-oss <energiaverdewasabidemo@gmail.com>
2026-03-23 21:04:34 +0100

    Add ScrollToTop for route changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import { StrictMode } from 'react';
+import { StrictMode, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import App from './App.tsx';
 import SubirFactura from './pages/SubirFactura.tsx';
 import AfiliadosLogin from './pages/afiliados/Login.tsx';
@@ -8,6 +8,14 @@ import AfiliadosRegistro from './pages/afiliados/Registro.tsx';
 import AfiliadosPanel from './pages/afiliados/Panel.tsx';
 import { AfiliadosAuthProvider } from './lib/afiliadosAuth.tsx';
 import './index.css';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'instant' as ScrollBehavior });
+  }, [pathname]);
+  return null;
+}
 
 function SubirFacturaWrapper() {
   const navigate = useNavigate();
@@ -27,6 +35,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AfiliadosAuthProvider>
+        <ScrollToTop />
         <Routes>
           <Route path="/subir-factura" element={<SubirFacturaWrapper />} />
           <Route path="/afiliados" element={<AfiliadosPanel />} />
